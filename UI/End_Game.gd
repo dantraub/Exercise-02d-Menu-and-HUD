e80ef8af3e923b14741b5e84c2885b42c7dbf1c1
dantraub <97706613+dantraub@users.noreply.github.com>
2022-02-08 22:57:15 -0500

    Completes the exercise

---
+++ UI/End_Game.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _ready():
+	$Label.text = "Thanks for playing! Your final score was " + str(Global.score) + "."
+	pass
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+	pass 
+
+func _on_Play_Again_pressed():
+	Global.reset()
+	var _scene = get_tree().change_scene("res://Game.tscn")
+	pass 
